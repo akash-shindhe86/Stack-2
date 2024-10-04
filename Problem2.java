afa727c1c45115c71438e5e1189ae3f117b0c1e3
ashin19 <ashin19@safeway.com>
2024-10-04 10:13:01 -0700

    done

---
+++ Problem2.java
@@ -0,0 +1,23 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n) stack
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i ++){
+            char c = s.charAt(i);
+            if(c == '{'){
+                st.push('}');
+            }else if (c == '('){
+                st.push(')');
+            }else if(c == '['){
+                st.push(']');
+            }else if (st.isEmpty() || st.pop() != c){
+                return false;
+            }
+        }
+        if(!st.isEmpty()) return false;
+
+        return true;
+    }
+}
